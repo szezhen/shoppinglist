765fe381c39bc11b51344ebaaccf68c822446c6c
szezhen.wong <szezhen94@gmail.com>
2019-05-25 15:52:23 +0800

    initial commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-  const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
   // Check for token
   if (!token)
-    return res.status(401).json({ msg: 'No token, authorizaton denied' });
+    return res.status(401).json({ msg: "No token, authorizaton denied" });
 
   try {
     // Verify token
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, process.env.jwtSecret);
     // Add user from payload
     req.user = decoded;
     next();
   } catch (e) {
-    res.status(400).json({ msg: 'Token is not valid' });
+    res.status(400).json({ msg: "Token is not valid" });
   }
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const config = require('config');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
 
 const app = express();
 
@@ -9,29 +9,29 @@ const app = express();
 app.use(express.json());
 
 // DB Config
-const db = config.get('mongoURI');
+const db = process.env.MONGO_URI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, { 
+  .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
   }) // Adding new mongo url parser
-  .then(() => console.log('MongoDB Connected...'))
+  .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', require('./routes/api/items'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
